fba2235db006130d5c9afaba0e15a346a2228f79
Ian Park <113217240+ip3143@users.noreply.github.com>
2024-11-22 11:14:49 +0900

    Add files via upload
    Original game, to be expanded

---
+++ src/main/GamePanel.java
@@ -0,0 +1,133 @@
+package main;
+
+import entity.Player;
+import object.SuperObject;
+import tile.TileManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.security.Key;
+
+public class GamePanel extends JPanel implements Runnable{
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16 x 16
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale; // 48 x 48
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768
+    public final int screenHeight = tileSize * maxScreenRow; // 576
+
+    // WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+
+    KeyHandler keyH = new KeyHandler();
+    Sound se = new Sound();
+    Sound music = new Sound();
+
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
+    Thread gameThread;
+    public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void setupGame() {
+        aSetter.setObject();
+
+        playMusic(0);
+    }
+
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null) {
+
+            update();
+
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        // TILE
+        tileM.draw(g2);
+
+        // OBJECT
+        for (int i = 0; i < obj.length; i++) {
+            if(obj[i] != null) {
+                obj[i].draw(g2, this);
+            }
+        }
+
+        // PLAYER
+        player.draw(g2);
+
+        // UI
+        ui.draw(g2);
+
+        g2.dispose();
+    }
+
+    public void playMusic(int i){
+
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+    public void playSE(int i){
+
+        se.setFile(i);
+        se.play();
+    }
+}
